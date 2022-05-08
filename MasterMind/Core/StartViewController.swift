9f4cde963f8d7bd894058848c2e1a59c24850dc9
sbrick26 <swayambarik@gmail.com>
2022-05-08 15:03:40 -0700

    start view comments

--- MasterMind/Core/StartViewController.swift
+++ MasterMind/Core/StartViewController.swift
@@ -5,21 +5,31 @@
 //  Created by Swayam Barik on 5/8/22.
 //
 
+/*
+ 
+Start View Controller: This view controller is the start point for the app
+
+*/
+
 import UIKit
 
 class StartViewController: UIViewController {
+    
     @IBOutlet weak var titleLabel: UILabel!
     
+    //startGameButton with segue in Main.storyboard to main View Controller
     @IBOutlet weak var startGameButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Set Start View Design
         
         view.backgroundColor = .black
         titleLabel.textColor = .green
         startGameButton.titleLabel?.textColor = .black
         startGameButton.setTitleColor(.black, for: .normal)
         startGameButton.setTitleColor(.black, for: .selected)
-        // Do any additional setup after loading the view.
     }
 
 }
